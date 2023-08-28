014eb2027dc327c9e0908770f89274b231f16b6b
Yurii <yurii38467@gmail.com>
2023-08-28 06:30:13 -0700

    Added include path for TPS folder

--- Source/TPS/TPS.Build.cs
+++ Source/TPS/TPS.Build.cs
@@ -7,14 +7,15 @@ public class TPS : ModuleRules
 {
     public TPS(ReadOnlyTargetRules Target) : base(Target)
     {
+        /*
         Console.WriteLine("TPS module -------------->");
         Console.WriteLine(
             "Unreal version {0}.{1}.{2}", Target.Version.MajorVersion, Target.Version.MinorVersion, Target.Version.PatchVersion);
         Console.WriteLine("Target platform {0}", Target.Platform);
         Console.WriteLine("Target type {0}", Target.Type);
-        Console.WriteLine("Branch name {0}", Target.Version.BranchName);
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        Console.WriteLine("Branch name {0}", Target.Version.BranchName);*/
 
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
         if (Target.Type == TargetType.Editor)
@@ -26,5 +27,7 @@ public class TPS : ModuleRules
         {
             PublicDefinitions.Add("MY_VR_MACRO=0");
         }
+
+        PublicIncludePaths.Add("TPS");
     }
 }
